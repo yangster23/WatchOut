f55131ce65c42bdc754aba358d2496ea662b4f89
Jonathan Yang <j23eagle@gmail.com>
2016-11-08 18:13:28 -0500

    Added Button

--- app/src/main/java/com/example/jonathanyang/watchout/MainActivity.java
+++ app/src/main/java/com/example/jonathanyang/watchout/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.jonathanyang.watchout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { // Main class for application
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+
+        Button firstButton = (Button) findViewById(R.id.firstButton);
+
+        firstButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstTextView.setText("You Clicked!");
+            }
+        });
     }
 }
