2eca29bd4bcef5bc0aba2dc8b01d3a96367dc7c9
Jonathan Yang <j23eagle@gmail.com>
2016-12-06 19:30:17 -0500

    Updated Location with Run-Time Permission, Speed of the Device. Refactored Version currently not working with permissions so doing it on Main Activity atm.

--- app/src/main/java/com/example/jonathanyang/watchout/MainActivity.java
+++ app/src/main/java/com/example/jonathanyang/watchout/MainActivity.java
@@ -1,22 +1,38 @@
 package com.example.jonathanyang.watchout;
 
-
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
 import java.text.DateFormat;
 import java.util.Date;
 
-/* Main Page of the Application.
- */
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener { // Main class for application
 
-public class MainActivity extends AppCompatActivity implements LocationProvider.LocationCallback { // Main class for application
-
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
     public static final String TAG = MainActivity.class.getSimpleName();
-    private LocationProvider mLocationProvider;
-    // Declare our View Variables
+    private final static int MY_PERMISSION = 123;
     private TextView mLongitudeView;
     private TextView mLatitudeView;
     private TextView mLastUpdateView;
@@ -25,8 +41,18 @@ public class MainActivity extends AppCompatActivity implements LocationProvider.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
-        mLocationProvider = new LocationProvider(this, this);
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(3* 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in milliseconds
 
         // Assign the View from the layout file to the corresponding variables
         mLongitudeView = (TextView) findViewById(R.id.longitudeView);
@@ -38,19 +64,91 @@ public class MainActivity extends AppCompatActivity implements LocationProvider.
         Log.d(TAG, location.toString());
         mLongitudeView.setText(String.valueOf(location.getLatitude()));
         mLatitudeView.setText(String.valueOf(location.getLongitude()));
-        mLastUpdateView.setText(DateFormat.getTimeInstance().format(new Date()));
+        //mLastUpdateView.setText(DateFormat.getTimeInstance().format(new Date()));
+        mLastUpdateView.setText(String.valueOf(location.getSpeed()));
+        Log.d(TAG, String.valueOf(location.getSpeed()));
+    }
+
+    @Override
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mLocationProvider.connect();
+        mGoogleApiClient.connect();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        mLocationProvider.disconnect();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
     }
 
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION);
+        } else {
+            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+            handleNewLocation(mLastLocation);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if (requestCode == MY_PERMISSION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission was granted, yay! Do the
+                // contacts-related task you need to do.
+                Log.d(TAG, "Granted Permission");
+                Toast.makeText(MainActivity.this, "ACCESS_FINE_LOCATION Accessed", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+        else {
+            // Permission Denied
+            Log.d(TAG, "Denied Permission");
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.i(TAG, "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        handleNewLocation(location);
+    }
 }
